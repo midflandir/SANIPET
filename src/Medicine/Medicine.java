a6707570139aec5aff94a8429021d9febca0284f
midflandir <juancamilo372cp@live.com>
2022-07-07 07:35:58 -0500

    Medicine added

---
+++ src/Medicine/Medicine.java
@@ -0,0 +1,13 @@
+package Medicine;
+
+public class Medicine {
+
+    private String Name;
+    private String presentation;
+    private String usage;
+    private Double quantity;
+    private String quantitytype;
+    private Integer stock;
+
+
+}
